cab042953c15637c0f9ebe32eb11febd914d1e6b
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-14 19:53:33 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -14,15 +14,6 @@ class Board
     @@cells.clear
   end
 
-
-  def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
 def position(move)
    position = move.to_i - 1
    position
