ab9fa3f390b9449956f2ca98763910fecaff9f78
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-14 20:00:20 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -11,13 +11,12 @@ class Board
     @@cells = Array.new(9, " ")
   end
 
-  def reset!
-    @@cells.clear
-  end
-
-def position(move)
-   position = move.to_i - 1
-   position
+  def display
+  puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+  puts "-----------"
+  puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+  puts "-----------"
+  puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
 end
 
 end
