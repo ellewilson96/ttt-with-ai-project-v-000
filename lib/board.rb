0552d5186de1a3813600d541c51bc5159b89a370
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-14 19:11:28 +0000

    automatically backed up by learn

---
+++ lib/board.rb
@@ -0,0 +1,20 @@
+class Board
+
+  @@cells = []
+
+  def initialize(board)
+  end
+
+  def reset!
+    @@cells.clear
+  end
+
+  def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+end
