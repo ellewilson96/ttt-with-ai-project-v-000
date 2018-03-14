5556ce752f724475b476f939fa21e7d639fc488c
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-14 21:48:53 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -22,6 +22,8 @@ class Game
     WIN_COMBINATIONS.detect do |combo|
       @board.cells[combo[0]] == @board.cells[combo[1]] &&
       @board.cells[combo[1]] == @board.cells[combo[2]] &&
+      @board.taken?(combo[0]+1)
+    end
   end
 
   def draw?
