eedbfe28217b6245565030f4e4ed0e16e8639bf6
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-14 21:57:25 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -38,7 +38,19 @@ class Game
     end
   end
 
-  def self.turn
-    
-
+  def turn
+     player = current_player
+     current_move = player.move(@board)
+     if !@board.valid_move?(current_move)
+       turn
+     else
+       puts "Turn: #{@board.turn_count+1}\n"
+       @board.display
+       @board.update(current_move, player)
+       puts "#{player.token} moved #{current_move}"
+       @board.display
+       puts "\n\n"
+     end
+   end
+   
 end
